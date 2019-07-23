bfeece80ed50235d4de8594d29abce4132c6766c
Gabriel Jacome <gabrieljacome37@outlook.com>
2019-07-23 11:17:06 -0300

    fazendo aula 2.1 e 2.2

---
+++ aula2.1.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int main(){
+
+    tipo nome;
+    char    
+    int
+    float
+    double
+
+    return 0;
+}
---
+++ aula2.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char letra = 'a';
+    char nro = 10;
+    int nro = 10;
+    float n = 5.25;
+    double d = 5.0e3;
+
+    int nro1 = 10;
+    int nro2;
+    int nro3;
+    int nro1 = 10, nro2, nro3;
+    
+    return 0;
+}
