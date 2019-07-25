1efd92132a6721907a84cd5a954b3f6c32d34b84
Gabriel Jacome <gabrieljacome37@outlook.com>
2019-07-25 11:13:40 -0300

    fazendo aula 4.2.c

---
+++ aula4.2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int nro;
+    float f;
+
+    printf("digite dois numeros: ");
+    scanf("%d %f", &nro, &f);
+    printf("\n numeros: %d e %f\n",nro,f);
+
+    return 0;
+}
