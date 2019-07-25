6c3010e1b2a27965012f9607ea7fa13e0d334f54
Gabriel Jacome <gabrieljacome37@outlook.com>
2019-07-25 10:13:07 -0300

    fazendo aula3.2.c

--- aula3.2.c
+++ aula3.2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int main()[
+int main(){
 
     char letra = 'a';       
     int nro = 10;
     float n = 5.25;
 
     printf("%c \n",letra);
-    printf("%d \n",valor da letra);
+    printf("%d \n",letra);
     printf("%d \n",nro);
     printf("%f \n",n);
 
 return 0;
-]
+}
