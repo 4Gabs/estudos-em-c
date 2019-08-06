c5f949d7e828dfdf614c47455e8265f310232a49
Gabriel Jacome <gabrieljacome37@outlook.com>
2019-08-06 10:54:44 -0300

    fazendo aula9.4.c

---
+++ aula9.4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+    int x = 10, y = 10;
+    
+    y = ++x;
+    // x++;
+    // y = x;
+    printf("\n x = %d \n",x);
+    printf("\n y = %d \n",y);
+
+    return 0;
+}
