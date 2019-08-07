492d333cacd7a8b5956b6b0cb1452995ce292518
Gabriel Jacome <gabrieljacome37@outlook.com>
2019-08-07 10:38:09 -0300

    fazendo aula10.2.c

---
+++ aula10.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+    //subtração simplificada
+    int x = 10, y = 10;
+    int z = 2;
+
+    x = x - z;
+    printf("\n x = %d \n",x);
+
+    y -= z; //y = y -z
+    printf("\n y = %d \n",y);
+
+    return 0;
+}
