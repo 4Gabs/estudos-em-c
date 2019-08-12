97c6f02d58dc9ced781b00f1db6db347f2bc952f
Gabriel Jacome <gabrieljacome37@outlook.com>
2019-08-12 10:49:05 -0300

    fazendo aula11.2.c

---
+++ aula11.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+
+    int x = 5, y = 3;
+
+    printf("\n Resultado = %d \n", x == 4);
+    printf("\n Resultado = %d \n", x - 2 != y);
+    printf("\n Resultado = %d \n", x == 4);
+
+    return 0;
+}
