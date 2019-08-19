8a149cd57d032981d9d0be40faf0d67571c0c1ed
Gabriel Jacome <gabrieljacome37@outlook.com>
2019-08-19 16:16:24 -0300

    fazendo exercício3.c

---
+++ exercício3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+    int nro = 10;
+    char c = 'a';
+    int x;
+
+    printf("\n digite dois numeros:\n");
+   
+    printf("\n os numeros sao: %d e %d \n",nro,c);
+
+    x = nro + c;
+    printf("\n soma = %d \n",x);
+
+    return 0;
+}
